a59a4ff8aca1e3336fd09ae7f688bba7b1f6adb0
Git-Hub452 <paudelsabina345@gmail.com>
2023-10-20 09:39:00 +0545

    product management system ui

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+
+
+import './Navbar.css'
+const Navbar = () => {
+  return (
+    <div className="navbar">
+    <a href="#home">Home</a>
+    <a href="#about">About</a>
+    <a href="#services">Services</a>
+    <a href="#contact">Contact</a>
+    <a class="icon" onclick="myFunction()">&#9776;</a></div>
+  )
+}
+
+export default Navbar
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,45 @@
+
+import { useEffect, useState } from 'react'
+import Navbar from '../../Components/Navbar'
+import './Home.css'
+import axios from 'axios'
+
+
+const Home = () => {
+  const [products,setProducts]= useState([])
+  const fetchProducts = async ()=>{
+     const response = await axios.get("https://653142f74d4c2e3f333cc097.mockapi.io/product")
+    setProducts (response.data)
+  }
+
+
+  useEffect(()=>{
+     fetchProducts ()
+  },[])
+  console.log(products)
+  return (
+    <>
+    <Navbar />
+    {
+      products.map((product) =>{
+        return(
+          <div key={product.id} className ="product-card">
+    <img src={product.productImage} alt="Product Image" />
+    <div className="product-details">
+        <h2 className="product-name">{product.productName}</h2>
+        <p className ="product-description">{product.productDescription}</p>
+    </div>
+</div>
+        )
+      })
+    }
+      
+    
+    
+</>
+    
+   
+  )
+}
+
+export default Home
